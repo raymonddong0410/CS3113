af315859afd15da3097b689ade7ebbd5384121fd
Raymond Dong <raymonddong0410@gmail.com>
2025-09-25 19:11:05 -0400

    Initial Base Code From Class

---
+++ Project 1/main.cpp
@@ -0,0 +1,134 @@
+/**
+* Author: Raymond Dong
+* Assignment: Simple 2D Scene
+* Date due: 2025-09-27, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#include "starter/cs3113.h"
+#include <math.h>
+
+// Global Constants
+constexpr int   SCREEN_WIDTH  = 1600,
+                SCREEN_HEIGHT = 900,
+                FPS           = 60,
+                SIZE          = 1000;
+constexpr float MAX_AMP       = 10.0f;
+
+constexpr char    BG_COLOUR[] = "#B2AAC6";
+constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+constexpr Vector2 BASE_SIZE   = { (float) SIZE, (float) SIZE };
+
+// Image owned by Nintendo — please don't sue me.
+constexpr char LINK_FP[]  = "assets/game/edelgard_square.png";
+
+// Global Variables
+AppStatus gAppStatus     = RUNNING;
+float     gScaleFactor   = SIZE,
+          gAngle         = 0.0f,
+          gPulseTime     = 0.0f;
+Vector2   gLinkPosition      = ORIGIN;
+Vector2   gLinkScale         = BASE_SIZE;
+float     gPreviousTicks = 0.0f;
+
+Texture2D gLinkTexture;
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
+
+// Function Definitions
+void initialise()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures");
+
+    gLinkTexture = LoadTexture(LINK_FP);
+
+    SetTargetFPS(FPS);
+}
+
+void processInput() 
+{
+    if (WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() 
+{
+    // Delta time
+    float ticks = (float) GetTime();
+    float deltaTime = ticks - gPreviousTicks;
+    gPreviousTicks = ticks;
+
+    gPulseTime += 1.0f * deltaTime;
+
+    gLinkScale = {
+        BASE_SIZE.x + MAX_AMP * cos(gPulseTime),
+        BASE_SIZE.y + MAX_AMP * cos(gPulseTime)
+    };
+}
+
+void render()
+{
+    BeginDrawing();
+    ClearBackground(ColorFromHex(BG_COLOUR));
+
+    // Whole texture (UV coordinates)
+    Rectangle textureArea = {
+        // top-left corner
+        0.0f, 0.0f,
+
+        // bottom-right corner (of texture)
+        static_cast<float>(gLinkTexture.width),
+        static_cast<float>(gLinkTexture.height)
+    };
+
+    // Destination rectangle – centred on gPosition
+    Rectangle destinationArea = {
+        gLinkPosition.x,
+        gLinkPosition.y,
+        static_cast<float>(gLinkScale.x),
+        static_cast<float>(gLinkScale.y)
+    };
+
+    // Origin inside the source texture (centre of the texture)
+    Vector2 objectOrigin = {
+        static_cast<float>(gLinkScale.x) / 2.0f,
+        static_cast<float>(gLinkScale.y) / 2.0f
+    };
+
+    // Render the texture on screen
+    DrawTexturePro(
+        gLinkTexture, 
+        textureArea, 
+        destinationArea, 
+        objectOrigin, 
+        gAngle, 
+        WHITE
+    );
+
+    EndDrawing();
+}
+
+void shutdown() { CloseWindow(); }
+
+int main(void)
+{
+    initialise();
+
+    while (gAppStatus == RUNNING)
+    {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
